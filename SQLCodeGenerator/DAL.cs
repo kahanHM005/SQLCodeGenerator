e7a4cad6ca6f77dd37a1ed1714b38605aac32a17
Manjoor Ali <manjoor@live.com>
2022-10-01 12:00:01 +0530

    Add project files.

---
+++ SQLCodeGenerator/DAL.cs
@@ -0,0 +1,119 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace SQLCodeGenerator
+{
+    public class DAL
+    {
+        public static string? ConnectionString { get; set; }
+
+        public static int Execute(String sQuery, CommandType type, params SqlParameter[] Params)
+        {
+            using (SqlConnection con = new SqlConnection(DAL.ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(sQuery, con))
+                {
+                    cmd.CommandType = type;
+                    foreach (SqlParameter prm in Params)
+                        cmd.Parameters.Add(prm);
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static SqlDataReader GetDataReader(String sQuery, params SqlParameter[] Params)
+        {
+            using (SqlConnection con = new SqlConnection(DAL.ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(sQuery, con))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    foreach (SqlParameter prm in Params)
+                        cmd.Parameters.Add(prm);
+                    return cmd.ExecuteReader();
+                }
+            }
+        }
+
+        public static SqlDataReader GetDataReader(String sQuery, CommandType ct, params SqlParameter[] Params)
+        {
+            using (SqlConnection con = new SqlConnection(DAL.ConnectionString))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand(sQuery, con))
+                {
+                    cmd.CommandType = ct;
+                    foreach (SqlParameter prm in Params)
+                        cmd.Parameters.Add(prm);
+                    return cmd.ExecuteReader();
+                }
+            }
+        }
+
+        public static DataSet GetDataSet(String sQuery, params SqlParameter[] Params)
+        {
+            using (SqlConnection con = new SqlConnection(DAL.ConnectionString))
+            {
+                con.Open();
+                using (SqlDataAdapter da = new SqlDataAdapter(sQuery, con))
+                {
+                    foreach (SqlParameter prm in Params)
+                        da.SelectCommand.Parameters.Add(prm);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    return ds;
+                }
+            }
+        }
+
+        public static SqlParameter CreateParameter(string Name, SqlDbType DBType, int Width, object prmValue)
+        {
+            SqlParameter NewPrm = new SqlParameter(Name, DBType, Width);
+            NewPrm.Value = prmValue;
+            return NewPrm;
+        }
+
+        public static DataSet GetDataSet(String sQuery, CommandType type, params SqlParameter[] Params)
+        {
+            using (SqlConnection con = new SqlConnection(DAL.ConnectionString))
+            {
+                con.Open();
+                using (SqlDataAdapter da = new SqlDataAdapter(sQuery, con))
+                {
+                    da.SelectCommand.CommandType = type;
+                    foreach (SqlParameter prm in Params)
+                        da.SelectCommand.Parameters.Add(prm);
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    return ds;
+                }
+            }
+        }
+        public static String GetNewPassword(bool UpperCase = true, bool LowerCase = true, bool digits = true, String SpecialChar = "#@")
+        {
+            var chars = "";
+
+            if (UpperCase)
+                chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            if (LowerCase)
+                chars += "abcdefghijklmnopqrstuvwxyz";
+
+            if (digits)
+                chars += "0123456789";
+
+            chars += SpecialChar;
+
+            var random = new Random();
+            String newPassword = new string(
+            Enumerable.Repeat(chars, 8)
+            .Select(s => s[random.Next(s.Length)])
+            .ToArray());
+            return newPassword;
+        }
+    }
+}
+
+
---
+++ SQLCodeGenerator/Program.cs
@@ -0,0 +1,132 @@
+﻿
+using SQLCodeGenerator;
+using System.Data;
+
+DAL.ConnectionString = "Data Source=.;Initial Catalog=Test;Integrated Security=True";
+int st = 0;
+bool q = false;
+while(!q)
+{
+    PrintMenu();
+    ConsoleKeyInfo c = Console.ReadKey();
+    switch(c.Key)
+    {
+        case ConsoleKey.Q:
+        case ConsoleKey.NumPad3:
+            q = true;
+            break;
+        case ConsoleKey.I:
+        case ConsoleKey.NumPad1:
+            st = 1;
+            break;
+        case ConsoleKey.U:
+        case ConsoleKey.NumPad2:
+            st = 2;
+            break;
+    }
+    if(!q)
+    {
+        Console.Write("\n\n\n\t Enter Table Name :  ");
+        string? t = Console.ReadLine();
+        if (!String.IsNullOrEmpty(t))
+        {
+            string script = GenerateScript(t, st);
+            Console.WriteLine("\n\n\n" + script);
+            Console.Write("\n\n\n\t   Press any key to go to main menu ");
+            Console.ReadKey();
+        }
+    }
+}
+
+static void PrintMenu()
+{
+    Console.Clear();
+    Console.WriteLine("******************Insert Update sql script generator**********************");
+    Console.WriteLine("\n\n\n\t1. (I) Insert Script ");
+    Console.WriteLine("\t2. (U) Update Script ");
+    Console.WriteLine("\t3. (Q) Quit ");
+    Console.Write("\n\t   Enter your choice : ");
+}
+
+static string GenerateScript(string? table, int type)
+{
+    string sql = "";
+    try
+    {
+        string query = @"SELECT COL.name AS ColumnName, ST.name as DataType, COL.max_length
+                            From sys.columns COL INNER JOIN sys.tables TAB 
+                            On COL.object_id = TAB.object_id INNER JOIN sys.types ST on ST.user_type_id = COL.user_type_id
+                            where TAB.name = '" + table + "'";
+
+        DataSet ds = DAL.GetDataSet(query);
+
+        if (ds.Tables[0].Rows.Count == 0)
+            return "Table " + table + " not found";
+
+        List<string> s1 = new List<string>();
+        List<string> s2 = new List<string>();
+        List<string> s3 = new List<string>();
+        List<string> s4 = new List<string>();
+        foreach (DataRow r in ds.Tables[0].Rows)
+        {
+            string t = "";
+            string? n = r["ColumnName"].ToString();
+            string? dt = r["DataType"].ToString();
+            string? l = r["max_length"].ToString();
+            s1.Add("[" + n + "]");
+            s3.Add("@" + n);
+            s4.Add("[" + n + "]" + " = " + "@" + n);
+            switch (dt)
+            {
+                case "bigint":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.BigInt,-1,vvv)";
+                    break;
+                case "bit":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.Bit,-1,vvv)";
+                    break;
+                case "char":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.Char," + l + ",vvv)";
+                    break;
+                case "datetime":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.DateTime,-1,vvv)";
+                    break;
+                case "decimal":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.Decimal,-1,vvv)";
+                    break;
+                case "float":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.Float,-1,vvv)";
+                    break;
+                case "int":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.Int,-1,vvv)";
+                    break;
+                case "nchar":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.NChar," + l + ",vvv)";
+                    break;
+                case "nvarchar":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.NVarChar," + l + ",vvv)";
+                    break;
+                case "smallint":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.SmallInt,-1,vvv)";
+                    break;
+                case "tinyint":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.TinyInt,-1,vvv)";
+                    break;
+                case "varchar":
+                    t = "DAL.CreateParameter(\"@" + n + "\",SqlDbType.VarChar," + l + ",vvv)";
+                    break;
+            }
+            s2.Add(t);
+        }
+        if (type == 1)
+            sql = "Insert Into " + table + "(" + String.Join(",", s1.ToArray())
+                + ") Values( " + String.Join(",", s3.ToArray()) + ")," + Environment.NewLine + String.Join("," + Environment.NewLine, s2.ToArray()) + ");";
+        else
+            sql = "Update " + table + " Set " + String.Join(",", s4.ToArray()) + " Where Col = @value," + Environment.NewLine
+                + String.Join("," + Environment.NewLine, s2.ToArray()) + ");";
+    }
+    catch(Exception ex)
+    {
+        return ex.ToString();
+    }
+    return sql;
+}
